58fe1d912bef619b809a6fa362a1b2b1413c6f9e
Zach Wood <zachrwood28@gmail.com>
2022-09-15 19:17:38 -0500

    Added join query for comprehensive data table.

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
+-- Create tables
 create table "housing" (
 	"state" varchar(5) not null primary key,
 	"median_home_price" integer not null
@@ -13,3 +14,20 @@ create table "income" (
 	"median_income" integer not null,
 	"avg_income_growth_rate" decimal(5,4)
 );
+
+-- Join tables together
+-- NOTE: Only run this after loading data from pandas
+select housing.state,
+	housing.median_home_price,
+	poverty.poverty_rate,
+	income.median_income,
+	income.avg_income_growth_rate
+from housing
+inner join poverty on
+housing.state = poverty.state
+inner join income on
+housing.state = income.state;
+
+	
+	
+
